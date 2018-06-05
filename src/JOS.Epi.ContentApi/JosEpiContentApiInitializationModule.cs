f202e1e538b43969798afbc358ff36900bb45610
Josef Ottosson <i@josef.guru>
2018-06-05 14:00:51 +0200

    Removes HttpModule and uses RoutedContentEvent instead

--- src/JOS.Epi.ContentApi/JosEpiContentApiInitializationModule.cs
+++ src/JOS.Epi.ContentApi/JosEpiContentApiInitializationModule.cs
@@ -1,6 +1,11 @@
-﻿using EPiServer.Framework;
+﻿using System.Linq;
+using System.Web;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework;
 using EPiServer.Framework.Initialization;
 using EPiServer.ServiceLocation;
+using EPiServer.Web.Routing;
 using JOS.Epi.ContentApi.Internal;
 
 namespace JOS.Epi.ContentApi
@@ -9,7 +14,43 @@ namespace JOS.Epi.ContentApi
     [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
     public class JosEpiContentApiInitializationModule : IConfigurableModule
     {
-        public void Initialize(InitializationEngine context) {}
+        public void Initialize(InitializationEngine context)
+        {
+            var events = ServiceLocator.Current.GetInstance<IContentRouteEvents>();
+            events.RoutedContent += OnRoutedContent;
+        }
+
+        private void OnRoutedContent(object sender, RoutingEventArgs e)
+        {
+            var httpContext = HttpContext.Current;
+            if (httpContext == null)
+            {
+                return;
+            }
+            var shouldSerializeStrategy = ServiceLocator.Current.GetInstance<IShouldSerializeResponseStrategy>();
+            if (!shouldSerializeStrategy.Execute(httpContext.Request))
+            {
+                return;
+            }
+
+            var contentLink = e.RoutingSegmentContext.RoutedContentLink;
+            var contentLoader = ServiceLocator.Current.GetInstance<IContentLoader>();
+            contentLoader.TryGet<IContent>(contentLink, out var content);
+
+            if (content == null)
+            {
+                return;
+            }
+
+            e.CancelFurtherRouting = true;
+            var serializer = ServiceLocator.Current.GetInstance<IContentApiSerializer>();
+            var result = serializer.Serialize(content);
+
+            httpContext.Response.ContentType = httpContext.Request.AcceptTypes?.First();
+            httpContext.Response.Write(result);
+            httpContext.Response.Flush();
+            httpContext.Response.End();
+        }
 
         public void Uninitialize(InitializationEngine context) {}
 
